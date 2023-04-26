e76856735be30ae06849f386d642ecdd5f3b5c57
dayannealcantara <dayannealcantara14@gmail.com>
2023-04-26 14:39:34 -0300

    feat:refacroring code

--- src/services/aws.js
+++ src/services/aws.js
@@ -1,70 +1,66 @@
-import { S3Client, CreateBucketCommand, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
 
 export default {
-  IAM_USER_KEY: '',
-  IAM_USER_SECRET: '',
-  BUCKET_NAME: '',
-  AWS_REGION: '',
+ 
   uploadToS3: function (file, filename, acl = 'public-read') {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       let IAM_USER_KEY = this.IAM_USER_KEY;
       let IAM_USER_SECRET = this.IAM_USER_SECRET;
       let BUCKET_NAME = this.BUCKET_NAME;
-      let AWS_REGION = this.AWS_REGION;
 
-      const s3client = new S3Client({
-        region: AWS_REGION,
-        credentials: {
-          accessKeyId: IAM_USER_KEY,
-          secretAccessKey: IAM_USER_SECRET,
-        },
+      let s3bucket = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        Bucket: BUCKET_NAME,
       });
 
-      try {
-        await s3client.send(new CreateBucketCommand({ Bucket: BUCKET_NAME }));
-
-        const params = {
+      s3bucket.createBucket(function () {
+        var params = {
           Bucket: BUCKET_NAME,
           Key: filename,
           Body: file.data,
-          ACL: acl,
+          // ACL: acl,
         };
 
-        const result = await s3client.send(new PutObjectCommand(params));
-
-        console.log(result);
-        return resolve({ error: false, message: result });
-      } catch (err) {
-        console.log(err);
-        return resolve({ error: true, message: err });
-      }
+        s3bucket.upload(params, function (err, data) {
+          if (err) {
+            console.log(err);
+            return resolve({ error: true, message: err });
+          }
+          console.log(data);
+          return resolve({ error: false, message: data });
+        });
+      });
     });
   },
   deleteFileS3: function (key) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       let IAM_USER_KEY = this.IAM_USER_KEY;
       let IAM_USER_SECRET = this.IAM_USER_SECRET;
       let BUCKET_NAME = this.BUCKET_NAME;
-      let AWS_REGION = this.AWS_REGION;
 
-      const s3client = new S3Client({
-        region: AWS_REGION,
-        credentials: {
-          accessKeyId: IAM_USER_KEY,
-          secretAccessKey: IAM_USER_SECRET,
-        },
+      let s3bucket = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        Bucket: BUCKET_NAME,
       });
 
-      try {
-        const result = await s3client.send(new DeleteObjectCommand({ Bucket: BUCKET_NAME, Key: key }));
-
-        console.log(result);
-        return resolve({ error: false, message: result });
-      } catch (err) {
-        console.log(err);
-        return resolve({ error: true, message: err });
-      }
+      s3bucket.createBucket(function () {
+        s3bucket.deleteObject(
+          {
+            Bucket: BUCKET_NAME,
+            Key: key,
+          },
+          function (err, data) {
+            if (err) {
+              console.log(err);
+              return resolve({ error: true, message: err });
+            }
+            console.log(data);
+            return resolve({ error: false, message: data });
+          }
+        );
+      });
     });
   },
 };
-
