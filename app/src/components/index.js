147d1853ea09195c7a018c43819512f0473ed41a
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-08 16:56:46 -0300

    feat: developing the splash screen, tour 1

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components/native";
 
+import { Title as TitlePaper, Text as TextPaper, Button as ButtonPaper } from "react-native-paper";
+
  export const Box = styled.View`
     flex:1;
     flex-wrap: ${(props) => props.wrap || 'nowrap'};
@@ -21,4 +23,55 @@ import styled from "styled-components/native";
     border:${(props) => props.border || 'none'};
 
     background:${(props) => props.theme[props.background] || props.background ||'transparent'};
-`
+`;
+
+export const Spacer = styled.View`
+  width: 100%;
+  height: ${(props) => props.size || '10px'};
+`;
+
+export const Title = styled(TitlePaper)`
+  color: ${(props) => props.theme[props.color || 'dark']};
+  font-size: ${(props) => (props.small ? '22px' : props.big ? '50px' : '30px')};
+  padding: ${(props) => (props.hasPadding ? '20px' : '0px')};
+  letter-spacing: -0.8px;
+  font-weight: ${(props) => (props.bold ? 'bold' : 'normal')};
+  line-height: ${(props) =>
+    props.small ? '22px' : props.big ? '50px' : '30px'};
+  text-align: ${(props) => props.align || 'left'};
+  transform: ${(props) => (props.scale ? `scale(${props.scale})` : '')};
+  font-family:"Ubuntu_700Bold";
+`;
+
+export const Cover = styled.ImageBackground.attrs((props) => ({
+    resizeMode: props.mode || 'contain',
+}))`
+    width: ${(props) => props.width || '100px'};
+    height: ${(props) => props.height || '100px'};
+    margin: ${(props) => props.spacing || '0px'};
+` 
+export const Text = styled(TextPaper)`
+  color: ${(props) => props.theme[props.color || 'muted']};
+  font-size: ${(props) => (props.small ? '13px' : '17px')};
+  font-family: ${(props) =>
+    props.bold ? 'Ubuntu_700Bold' : 'Ubuntu_300Light'};
+  margin: ${(props) => props.spacing || 0};
+  padding: ${(props) => (props.hasPadding ? '20px' : '0px')};
+  line-height: ${(props) => (props.small ? '13px' : '20px')};
+  text-decoration: ${(props) => (props.underline ? 'underline' : 'none')};
+  opacity: 0.7;
+  text-align: ${(props) => props.align || 'left'};
+`;
+
+export const Button = styled(ButtonPaper).attrs((props) => ({
+    color:
+      props.theme[props.background] || props.background || props.theme.primary,
+    width: props.block ? '100%' : 'auto',
+    labelStyle: {
+      color: props.theme[props.textColor || 'light'],
+      letterSpacing: 0,
+      fontFamily: 'Ubuntu_400Regular',
+    },
+    mode: 'contained',
+  }))``;
+  
--- app/src/pages/Tour/index.js
+++ app/src/pages/Tour/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {  Text } from 'react-native';
-import { Box } from '../../components';
+
+import { Box, Cover, Spacer, Title, Text, Button } from '../../components';
+
+import tour01 from '../../assets/illustration-1.png'
 
 const Tour = () => {
+  
   return (
-    <Box background="primary">
-      <Text>Tela de Tour</Text>     
+    <Box background="dark" hasPadding justify="center">
+      <Spacer size="40px"/>
+      <Title big color="light">Chega de rotina sem motivação.</Title> 
+      <Spacer size="50px"/> 
+      <Cover source={tour01} width="100%" height="300px" /> 
+      <Spacer size="50px"/> 
+      <Text align="center" small hasPadding>Pensamos em  um serviço perfeito para você construir hábitos se divertindo e ganhando dinheiro.</Text>
+      <Spacer size="50px"/> 
+      <Button >Próximo</Button>
+    
     </Box>
   );
 };
