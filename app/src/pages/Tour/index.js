7eb0e91d5c47f304ab2fea1d447765bf5da119ea
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-09 09:42:28 -0300

    feat: Making tour screens dynamic

--- app/src/pages/Tour/index.js
+++ app/src/pages/Tour/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Box, Cover, Spacer, Title, Text, Button } from '../../components';
 
 import tour01 from '../../assets/illustration-1.png'
+import tour02 from '../../assets/illustration-2.png'
+import tour03 from '../../assets/illustration-3.png'
 
 const Tour = () => {
+  const tourData = [
+    {
+      bg:'dark',
+      button:'primary',
+      title:'Chega de rotina sem a motivação.',
+      desc:'Pensamos em  um serviço perfeito para você construir       hábitos se divertindo e ganhando dinheiro.',
+      pic: tour01,
+    },
+    {
+      bg:'info',
+      button:'dark',
+      title:'Construa hábitos por bem (ou mal).',
+      desc:'Pensamos em  um serviço perfeito para você construir     hábitos se divertindo e ganhando dinheiro.',
+      pic: tour02,
+    },
+    {
+      bg:'dark',
+      button:'primary',
+      title:'Ganhe dinheiro com os seus amigos.',
+      desc:'Pensamos em  um serviço perfeito para você construir       hábitos se divertindo e ganhando dinheiro.',
+      pic: tour03,
+    },
+  ];
+
+  const [actualTour, setActualTour] = useState(0)
   
   return (
-    <Box background="dark" hasPadding justify="center">
+    <Box background={tourData[actualTour]?.bg} hasPadding justify="center">
       <Spacer size="40px"/>
-      <Title big color="light">Chega de rotina sem motivação.</Title> 
+      <Title big color="light">{tourData[actualTour]?.title}</Title> 
       <Spacer size="50px"/> 
-      <Cover source={tour01} width="100%" height="300px" /> 
+      <Cover source={tourData[actualTour]?.pic} width="100%" height="300px" /> 
       <Spacer size="50px"/> 
-      <Text align="center" small hasPadding>Pensamos em  um serviço perfeito para você construir hábitos se divertindo e ganhando dinheiro.</Text>
+      <Text align="center" small hasPadding>{tourData[actualTour]?.desc}</Text>
       <Spacer size="50px"/> 
-      <Button >Próximo</Button>
+      <Button block background={tourData[actualTour]?.button} onPress={()=> setActualTour(actualTour + 1)}>{actualTour === 2 ? 'Explorar' : 'Próximo'}</Button>
     
     </Box>
   );
