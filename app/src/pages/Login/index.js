05b5f4af75feee3e21ff3711c5fa719ae4293349
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-09 11:39:35 -0300

    feat: Importing invite modal

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalLogin, {modalRef as modalLoginRef} from '../../components/modal/login';
+import ModalInvite, {modalRef as modalInviteRef} from '../../components/modal/invite';
 import { Box, Cover, Spacer, Title, Text, Button } from '../../components';
 import logo from "../../assets/logo.png"
 
@@ -8,13 +9,14 @@ const Login = () => {
   return (
     <>
     <ModalLogin/>
+    <ModalInvite/>
       <Box background="dark" hasPadding  align="center">
         <Spacer size="150px"/>
         <Cover source={logo} width="80%" height="300px"/>
         <Spacer size="80px"/>
         <Button block onPress={() => modalLoginRef.current?.open()}>Entrar na minha conta</Button>
         <Spacer/>
-        <Button  mode='text' block>Pedir convite</Button>
+        <Button  mode='text' block onPress={() => modalInviteRef.current?.open()}>Pedir convite</Button>
         <Spacer size="50px"/>
         <Text small hasPadding align="center">Ao fazer login você concorda com nossos <Text small color="info">termos  e condições</Text>  e <Text small color="info">política de privacidade.</Text></Text>     
       </Box>
