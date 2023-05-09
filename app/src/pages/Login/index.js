a0efdd2f84592a95f9205004a233fc3882d42ea3
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-09 11:05:19 -0300

    feat: Importing login modal in the login screen

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-
+import ModalLogin, {modalRef as modalLoginRef} from '../../components/modal/login';
 import { Box, Cover, Spacer, Title, Text, Button } from '../../components';
 import logo from "../../assets/logo.png"
 
 
 const Login = () => {   
   return (
-    <Box background="dark" hasPadding  align="center">
-       <Spacer size="150px"/>
-      <Cover source={logo} width="80%" height="300px"/>
-      <Spacer size="80px"/>
-      <Button block>Entrar na minha conta</Button>
-      <Spacer/>
-      <Button  mode='text' block>Pedir convite</Button>
-      <Spacer size="50px"/>
-      <Text small hasPadding align="center">Ao fazer login você concorda com nossos <Text small color="info">termos  e condições</Text>  e <Text small color="info">política de privacidade.</Text></Text>     
-    </Box>
+    <>
+    <ModalLogin/>
+      <Box background="dark" hasPadding  align="center">
+        <Spacer size="150px"/>
+        <Cover source={logo} width="80%" height="300px"/>
+        <Spacer size="80px"/>
+        <Button block onPress={() => modalLoginRef.current?.open()}>Entrar na minha conta</Button>
+        <Spacer/>
+        <Button  mode='text' block>Pedir convite</Button>
+        <Spacer size="50px"/>
+        <Text small hasPadding align="center">Ao fazer login você concorda com nossos <Text small color="info">termos  e condições</Text>  e <Text small color="info">política de privacidade.</Text></Text>     
+      </Box>
+    </>
   );
 };
 
